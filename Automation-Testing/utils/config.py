483330640f909ebaf60e81948cf7bbcfacf9b616
ConwenC29 <165813366+ConwenC29@users.noreply.github.com>
2026-04-10 11:01:17 +0200

    Update config.py
    uploaded from config.py

--- Automation-Testing/utils/config.py
+++ Automation-Testing/utils/config.py
@@ -1,1 +1,20 @@
-config.py
+import os
+
+class Config:
+    BASE_URL = "https://www.saucedemo.com/"
+
+    # Test Credentials
+    VALID_USERNAME = "standard_user"
+    VALID_PASSWORD = "secret_sauce"
+
+    INVALID_USERNAME = "invalid_user"
+    INVALID_PASSWORD = "wrong_password"
+
+    # Browser Settings
+    HEADLESS = False
+    BROWSER = "chromium"  # options: chromium, firefox, webkit
+
+    # Timeout
+    TIMEOUT = 5000
+
+config = Config()
