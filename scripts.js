0fc91e1037be565858f7926779d1531be46a4e83
ConwenC29 <165813366+ConwenC29@users.noreply.github.com>
2026-04-14 20:19:40 +0200

    Create scripts.js
    file creation

---
+++ scripts.js
@@ -0,0 +1,19 @@
+<script>
+  const slides = document.querySelectorAll(".cert-slide");
+  let index = 0;
+
+  function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+  }
+
+  document.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  });
+
+  document.querySelector(".prev").addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+  });
+</script>
